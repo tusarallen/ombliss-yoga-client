b90493e1f62cddb6b65b0b0ea085a8c27e810927
email <allentusar909@gmail.com>
2023-06-07 11:46:04 +0600

    project is on

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home/Home";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage>Error</ErrorPage>,
     children: [
       {
         path: "/",
